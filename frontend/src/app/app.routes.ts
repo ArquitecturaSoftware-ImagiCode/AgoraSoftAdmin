f6c298a05cad1b3cd816472da1abfcd1556ea019
Victor Peñaranda <114187555+VictorJ15@users.noreply.github.com>
2025-10-15 14:12:53 -0500

    Revert "Front con errores"
    This reverts commit 2c0979e04134f3de61e703f612890ad52db64fa8, reversing
    changes made to a1e94b334d80ba7f061174c8c701fc4bb00415fa.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -13,10 +13,6 @@ import { TesoreriaLayout } from './layouts/tesoreria-layout/tesoreria-layout';
 import { PlaneacionLayout } from './layouts/planeacion-layout/planeacion-layout';
 import { SoporteLayout } from './layouts/soporte-layout/soporte-layout';
 
-
-import { RegistroEmpleadoComponent } from './pages/empleados/registro-empleado/registro-empleado.component';
-import { ListaEmpleadosComponent } from './pages/empleados/lista-empleados/lista-empleados.component';
-
 export const routes: Routes = [
   { path: 'register', component: SignUpPage },
   { path: 'login', component: SignInPage },
@@ -30,16 +26,7 @@ export const routes: Routes = [
         path: 'dashboard',
         component: ArquitecturaDashboard,
       },
-
-      {
-        path: 'empleados',
-        children: [
-          { path: 'registro', component: RegistroEmpleadoComponent },
-          { path: 'lista', component: ListaEmpleadosComponent },
-        ],
-      },
        { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
-
     ],
   },
   {
@@ -94,7 +81,7 @@ export const routes: Routes = [
        { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
     ],
   },
-/*
+
 {
     path: 'admin',
     // admin layout is a standalone component (AdminLayoutComponent) - lazy load it
@@ -159,6 +146,6 @@ export const routes: Routes = [
   // default / fallback
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: '' }
-*/
+
 
 ];
